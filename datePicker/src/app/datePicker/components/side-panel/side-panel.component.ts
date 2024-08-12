2b22ca3586ce8c6829fb9c18291640642009de38
gayatri-l-rajput <rajputlalit591@gmail.com>
2024-08-13 00:07:21 +0530

    feat: side component current date integration

--- datePicker/src/app/datePicker/components/side-panel/side-panel.component.ts
+++ datePicker/src/app/datePicker/components/side-panel/side-panel.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
-  styleUrls: ['./side-panel.component.scss']
+  styleUrls: ['./side-panel.component.scss'],
 })
 export class SidePanelComponent {
-
+  weekday: string = '';
+  day: string = '';
+  month: string = '';
+  year: string = '';
+  @Input() set currentDateString(value: string | null) {
+    if (value) {
+      let splitData = value.split(',');
+      this.weekday = splitData[0].trim();
+      let dayMonth = splitData[1].trim().split(' ');
+      this.day = dayMonth[1];
+      this.month = dayMonth[0];
+      this.year = splitData[2].trim();
+    }
+  }
 }
--- datePicker/src/app/datePicker/containers/date-picker-container/date-picker-container.component.ts
+++ datePicker/src/app/datePicker/containers/date-picker-container/date-picker-container.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-date-picker-container',
   templateUrl: './date-picker-container.component.html',
-  styleUrls: ['./date-picker-container.component.scss']
+  styleUrls: ['./date-picker-container.component.scss'],
 })
-export class DatePickerContainerComponent {
-
+export class DatePickerContainerComponent implements OnInit {
+  currentDate: Date = new Date();
+  currentLocaleString$ = new BehaviorSubject<string>('');
+  ngOnInit(): void {
+    let currentStringData = this.currentDate.toLocaleDateString(undefined, {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+    console.log(currentStringData);
+    this.currentLocaleString$.next(currentStringData);
+  }
 }
